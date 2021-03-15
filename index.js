c5b3fb8519d25dd870d0353ac9225f63e5840278
gdreamer1 <gdadreamer@gmail.com>
2021-03-14 21:46:50 -0700

    Complete Assinment

---
+++ index.js
@@ -0,0 +1,40 @@
+function add(a,b) {
+    return (a + b);
+}
+add(491 + 491);
+
+function subtract(a,b) {
+    return (a - b);
+}
+subtract(274 - 137);
+
+function multiply(a,b) {
+    return (a * b);
+}
+multiply(104,253.5 * 2);
+
+function divide(a,b) {
+    return (a / b);
+}
+divide (164.8181818181818 / 2);
+
+function increment(n) {
+    return (++ n);
+}
+increment(190)
+
+function decrement(n) {
+    return (-- n)
+}
+decrement(100)
+
+function makeInt(string){
+    return parseInt(string, 10)
+}
+makeInt(poop)
+
+makeInt('2');
+
+function preserveDecimal(n){
+    return parseFloat(n)
+}
